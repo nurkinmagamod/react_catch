a486ad740ac712b91fae5de51906779889a47cc8
nurkinmagazmod <battchat@gmail.com>
2017-10-05 02:24:27 +0700

    add fish add fishObj adding func

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+    createFish(event) {
+        event.preventDefault();
+        const fish = {
+        	name: this.name.value,
+        	price: this.price.value,
+        	status: this.status.value,
+        	desc: this.desc.value,
+        	image: this.image.value,
+        }
+        console.log(fish);
+    }
 	render() {
 		return (
 			<form className='fish-edit' onSubmit={(e) => this.createFish(e)}>
-				<input type="text" placeholder='Fish Name'/>
-				<input type="text" placeholder='Fish Price'/>
+				<input ref={(input) => this.name = input} type="text" placeholder='Fish Name'/>
+				<input ref={(input) => this.price = input} type="text" placeholder='Fish Price'/>
 				<select name="" id="">
-					<option value="available">Fresh!</option>
+					<option ref={(input) => this.status = input} value="available">Fresh!</option>
 					<option value="unavailable">Sold Out!</option>
 				</select>
-				<input type="text" placeholder='Fish Status'/>
-				<input type="text" placeholder='Fish Desc'/>
-				<input type="text" placeholder='Fish Image'/>
+				<textarea ref={(input) => this.desc = input} placeholder='Fish Desc'></textarea>
+				<input ref={(input) => this.image = input} type="text" placeholder='Fish Image'/>
 				<button type='submit'>+ Add Item</button>
 			</form>
 		)
