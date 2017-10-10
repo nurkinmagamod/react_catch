8368837f09912cc72601f4f6577f821eb07abdd0
nurkinmagazmod <battchat@gmail.com>
2017-10-10 23:49:44 +0700

    add fish component

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -18,8 +18,8 @@ class AddFishForm extends React.Component {
 			<form ref = {(input) => this.fishForm = input} className='fish-edit' onSubmit={(e) => this.createFish(e)}>
 				<input ref={(input) => this.name = input} type="text" placeholder='Fish Name'/>
 				<input ref={(input) => this.price = input} type="text" placeholder='Fish Price'/>
-				<select name="" id="">
-					<option ref={(input) => this.status = input} value="available">Fresh!</option>
+				<select ref={(input) => this.status = input}>
+					<option value="available">Fresh!</option>
 					<option value="unavailable">Sold Out!</option>
 				</select>
 				<textarea ref={(input) => this.desc = input} placeholder='Fish Desc'></textarea>
--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
+
 class App extends React.Component {
 	constructor() {
 		super();
@@ -34,6 +36,14 @@ class App extends React.Component {
 			<div className='catch-of-the-day'>
 				<div className='menu'>
 					<Header tagline='Fresh Seafood Market' />
+					<ul className="list-of-fishes">
+						{
+							Object
+							.keys(this.state.fishes)
+							.map(key => <Fish key={key} details={this.state.fishes} />)
+
+						}
+					</ul>
 				</div>
 				<Order />
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
