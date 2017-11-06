730f6292bd50362afdb6aac93f58bf935ba733a3
nurkinmagazmod <battchat@gmail.com>
2017-11-06 23:31:51 +0700

    extend for Inventory comp

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,8 +7,20 @@ class Inventory extends React.Component {
 		this.renderInventory = this.renderInventory.bind(this); 
 	}
 	renderInventory(key) {
+		const fish = this.props.fishes[key];
 		return (
-			<p>{key}</p>
+			<div className="fish-edit" key={key}>
+				<input type="text" name="name" value={fish.name} placeholder="Fish Name"/>
+				<input type="text" name="price" value={fish.price} placeholder="Fish Price"/>
+				<select type="text" name="status" value={fish.status} placeholder="Fish Status">
+					<option value="available">Fresh</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+
+				<textarea type="text" name="desc" value={fish.desc} placeholder="Fish Desc">
+				</textarea>
+				<input type="text" name="image" value={fish.image} placeholder="Fish Image"/>
+			</div>
 			)
 	}
 	
